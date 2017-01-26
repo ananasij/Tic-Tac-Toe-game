3bb8c354e5301d37178626b11eb7dc175bba8fb4
Hanna Senkevich <hanna.senkevich@gmail.com>
2017-01-26 13:46:58 +0100

    Updating score and starting a new game when someone wins

--- main.js
+++ main.js
@@ -1,16 +1,53 @@
+var tictactoe;
+
 function init() {
-    var tictactoe = new Tictactoe();
+    tictactoe = new Tictactoe();
+    tictactoe.on('markSave', renderCell);
+    tictactoe.on('scoreUpdate', renderScore);
 
     $('.js-cell').on('click', function(e) {
         var cellIndex = $(e.target).data('index');
         tictactoe.saveInput(cellIndex);
     });
+
+    $('.js-play-again').on('click', function() {
+        nextGame();
+    });
 }
 
-function renderCell(cellIndex, value) {
+function renderCell(cellIndex) {
     var cellClass = '.js-cell-' + cellIndex;
     var $mark = $(cellClass).find('.js-mark');
-    $mark.text(value);
+
+    $mark.text(tictactoe.getCurrentMark());
+}
+
+function renderScore(score) {
+    var $endGame = $('.js-end-game');
+    var $scoreLine = $('.js-score');
+    var $scoreX = $('.js-score-x');
+    var $score0 = $('.js-score-0');
+    $scoreLine.text(score);
+    $scoreX.text(tictactoe.scoreX);
+    $score0.text(tictactoe.score0);
+    $endGame.addClass('overlay');
+    $endGame.removeClass('invisible');
+}
+
+function nextGame() {
+    clearField();
+    var $endGame = $('.js-end-game');
+    $endGame.removeClass('overlay');
+    $endGame.addClass('invisible');
+}
+
+function clearField() {
+    for (var i = 1; i <= 9; i++) {
+        var cellClass = '.js-cell-' + i;
+        var $mark = $(cellClass).find('.js-mark');
+
+        $mark.text('');
+    }
 }
 
 $(document).ready(init);
@@ -20,12 +57,19 @@ $(document).ready(init);
 var PLAYER_X = 'PLAYER_X';
 var PLAYER_0 = 'PLAYER_0';
 var FIELD_SIZE = 9;
+var WINNING_COMBINATIONS = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7],
+    [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
 
 function Tictactoe() {
     this.currentPlayer = PLAYER_X;
+    this.callbacks = {
+        markSave: null,
+        scoreUpdate: null
+    };
     this.field = [];
+    this.scoreX = 0;
+    this.score0 = 0;
     this.init();
-    console.log(this.field);
 }
 
 Tictactoe.prototype.init = function() {
@@ -35,25 +79,22 @@ Tictactoe.prototype.init = function() {
 };
 
 Tictactoe.prototype.saveInput = function(cellIndex) {
-    var mark;
-    if (this.currentPlayer === PLAYER_X) {
-        mark = 'X';
-    } else {
-        mark = 'O';
-    }
+    var mark = this.getCurrentMark();
     if (cellIndex && !this.field[cellIndex - 1]) {
         this.field[cellIndex - 1] = mark;
-        renderCell(cellIndex, mark);
+        this.trigger('markSave', cellIndex);
         this.processInput();
     }
 };
 
 Tictactoe.prototype.processInput = function() {
-    console.log(this.field);
-    // todo: Check if anyone won. If so reset the game
-    this.switchPlayer();
-    if (this.currentPlayer === PLAYER_0) {
-        this.computerTurn();
+    if (this.checkWin()) {
+        this.nextGame();
+    } else {
+        this.switchPlayer();
+        if (this.currentPlayer === PLAYER_0) {
+            this.computerTurn();
+        }
     }
 };
 
@@ -72,4 +113,65 @@ Tictactoe.prototype.computerTurn = function() {
             return;
         }
     }
+};
+
+Tictactoe.prototype.checkWin = function() {
+    var currentMarks;
+    var win;
+    currentMarks = this.getPlayerMarks(this.getCurrentMark());
+
+    for (var i = 0; i < WINNING_COMBINATIONS.length; i++) {
+        win = true;
+        for (var j = 0; j < 3; j++) {
+            if (!currentMarks.includes(WINNING_COMBINATIONS[i][j])) {
+                win = false;
+                break;
+            }
+        }
+        if (win) {
+            break;
+        }
+    }
+    return win;
+};
+
+Tictactoe.prototype.updateScore = function() {
+    if (this.currentPlayer === PLAYER_X) {
+        this.scoreX += 1;
+    } else {
+        this.score0 += 1;
+    }
+    this.trigger('scoreUpdate', (this.currentPlayer + ' wins!'));
+};
+
+Tictactoe.prototype.on = function(eventName, callback) {
+    this.callbacks[eventName] = callback;
+};
+
+Tictactoe.prototype.trigger = function(eventName, value) {
+    if (this.callbacks[eventName]) {
+        this.callbacks[eventName](value);
+    }
+};
+
+Tictactoe.prototype.getCurrentMark = function() {
+    if (this.currentPlayer === PLAYER_X) {
+        return 'X';
+    }
+    return '0';
+};
+
+Tictactoe.prototype.getPlayerMarks = function(currentMark) {
+    var marks = [];
+    for (var i = 0; i < this.field.length; i++) {
+        if (this.field[i] === currentMark) {
+            marks.push(i + 1);
+        }
+    }
+    return marks;
+};
+
+Tictactoe.prototype.nextGame = function() {
+    this.updateScore();
+    this.init();
 };
